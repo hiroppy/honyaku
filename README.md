942ac0bcc93e60aa9b76bbfa57d533835f250878
abouthiroppy <git@about-hiroppy.com>
2016-11-27 15:56:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <h1>honyaku</h1>
+</div>
+
+[![Build Status](https://travis-ci.org/abouthiroppy/honyaku.svg?branch=master)](https://travis-ci.org/abouthiroppy/honyaku)
+<!-- npm version badge: https://badge.fury.io/ -->
+
+## Install
+```
+$ npm i -g honyaku
+```
+
+## Usage
+```
+$ honyaku "awesome"
+# 驚くばかり
+$ honyaku "驚くばかり"
+# Awesome
+```
+
+alias: hyk
---
+++ bin/index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const translate = require('google-translate-api');
+
+const commands = process.argv.slice(2);
+
+if (commands[0] === '-v' || commands[0] === '--version') {
+  console.log(`Version: ${require('../package.json').version}`);
+  process.exit(0);
+}
+
+if (commands[0] === undefined) {
+  console.log('<honyaku | hyk> text');
+  process.exit(0);
+}
+
+const str = commands[0];
+let isJapanese = false;
+
+for (let i = 0; i < str.length; i++) {
+  const code = commands[0].charCodeAt(i);
+
+  if (code >= 256) {
+    isJapanese = true;
+    break;
+  }
+}
+
+Promise.resolve().then(() => {
+  const to = isJapanese ? 'en' : 'ja';
+
+  return translateByGoogle(str, to);
+}).then((res) => {
+  console.log(res.text);
+}).catch((err) => {
+  console.error(err);
+});
+
+/**
+ * @param {string} str
+ * @param {string} to - ja or en
+ */
+function translateByGoogle(str, to) {
+  return translate(str, {to: to});
+}
